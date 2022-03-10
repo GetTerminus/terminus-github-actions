21a6a25fd6af866bf7d7153293677c635c07c45e
dreinhardt-terminus <david.reinhardt@terminus.com>
2022-03-10 14:46:45 -0500

    feat: test validator

--- shell-scripts/input-validator.sh
+++ shell-scripts/input-validator.sh
@@ -13,7 +13,7 @@ function validateInput() {
    error_count=0
    for key in ${!arr[@]};
       do
-        if [ ! ${arr[$key]+_} ]
+        if [[ -z "${arr[$key]+unset}" ]]
         then
           error_count=$((error_count+1))
           error "${key} is empty."
