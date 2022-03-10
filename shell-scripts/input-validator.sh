9b3a0a05d409ccedbf615e2888e4ca5fc389538a
dreinhardt-terminus <david.reinhardt@terminus.com>
2022-03-10 16:17:30 -0500

    feat: test validator

--- shell-scripts/input-validator.sh
+++ shell-scripts/input-validator.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+debug () {
+  if [ -n "${VALIDATOR_DEBUG+x}" ]; then
+    echo -e "\033[33;1mDEBUG:\033[0m $1"
+  fi
+}
+
+if [ -n "${VALIDATOR_ECHO+x}" ]; then
+  set -x
+fi
+
 error () {
   echo -e "\033[31;1mERROR:\033[0m $1"
+}
+
+success () {
+  echo -e "\033[32;1mERROR:\033[0m $1"
 }
 
 info () {
@@ -9,19 +23,22 @@ info () {
 }
 
 function validateInput() {
-   local -n arr=$1
-   error_count=0
-   for key in "${!arr[@]}";
-      do
-        if [ -z "${arr[${key}]}" ]
-        then
-          error_count=$((error_count+1))
-          error "${key} is empty."
-        fi
-      done
+  debug "$1"
+  local -n arr=$1
+  error_count=0
+  for key in "${!arr[@]}";
+    do
+      if [ -z "${arr[${key}]}" ]
+      then
+        error_count=$((error_count+1))
+        error "${key} is empty."
+      fi
+    done
   if [ "$error_count" -gt 0 ]
   then
     info "For a list of required inputs, please check the corresponding directory in: https://github.com/GetTerminus/terminus-github-actions"
     exit 1
+  else
+    success "Input validation passed!"
   fi
 }
